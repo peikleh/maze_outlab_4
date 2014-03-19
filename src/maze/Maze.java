0c9ad24a711aae3c9ae05cba8a5e0b300d3def1d
Nevin <Nevin@Nevin_HP>
2014-03-18 22:13:47 -0600

    Program Works

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package maze;
 
@@ -14,63 +10,67 @@ import java.util.Scanner;
  * @author Nevin
  */
 public class Maze {
+
     private int startR;
     private int startC;
-      char[][] maze;
-       private int rows = 0;
-        private int columns = 0;
-        private int fillR = 0;
-     public int FindStartR(){
-         for (int j = 0; j < rows; j++) {
+    char[][] maze;
+    private int rows = 0;
+    private int columns = 0;
+    private int fillR = 0;
+
+    public int FindStartR() {//Finds the starting row
+        for (int j = 0; j < rows; j++) {
             for (int k = 0; k < columns; k++) {
-                 if(maze[j][k]=='S'){
-                      startR=j;
-                   }
-                } }  
-               System.out.println(startR);
-                     return startR;
-         
-     }
-     public int FindStartC(){
-         for (int j = 0; j < rows; j++) {
+                if (maze[j][k] == 'S') {
+                    startR = j;
+                }
+            }
+        }
+
+        return startR;
+
+    }
+
+    public int FindStartC() {//Finds the Starting Column
+        for (int j = 0; j < rows; j++) {
             for (int k = 0; k < columns; k++) {
-                 if(maze[j][k]=='S'){
-                      startC=k;
-                   }
-                } } 
-         System.out.println(startC);
-                     return startC;
-         
-     }
-    public boolean SolveMaze(int x, int y){
-        System.out.println("poo");
-        if(x<0||y<0||x>rows||y>=columns){
-            System.out.println("1");
+                if (maze[j][k] == 'S') {
+                    startC = k;
+                }
+            }
+        }
+
+        return startC;
+
+    }
+
+    public boolean SolveMaze(int x, int y) {//recursive method that solves the maze
+
+        if (x < 0 || y < 0 || x >= rows || y >= columns) {
+
             return false;
-        }else if(maze[x][y]=='*'||maze[x][y]=='.' ){
-            System.out.println("2");
+        } else if (maze[x][y] == '*' || maze[x][y] == '.' || maze[x][y] == '+') {
+
             return false;
-        }else if(maze[x][y]=='F'){
-            maze[x][y]='+';
-            System.out.println("3");
+        } else if (maze[x][y] == 'F') {
+
             return true;
-        }else{
-            System.out.println("4rf");
-            maze[x][y]='+';
-            if(SolveMaze(x-1,y)||SolveMaze(x+1,y)||SolveMaze(x,y-1)||SolveMaze(x,y+1)){
+        } else {
+
+            maze[x][y] = '+';
+            if (SolveMaze(x, y - 1) == true || SolveMaze(x, y + 1) == true || SolveMaze(x - 1, y) == true || SolveMaze(x + 1, y) == true) {
                 return true;
-            }else{
-                maze[x][y]='.';
+            } else {
+                maze[x][y] = '.';
                 return false;
             }
         }
-      
-    }
-    
 
-    public char[][] ReadTextFile() {
-        
-        String fileName = "C:/Users/Nevin/Documents/testtextfile.txt";
+    }
+
+    public char[][] ReadTextFile(String file) {//reads text file and mades it a 2d array
+
+        String fileName = file;
         try {
             Scanner scanner = new Scanner(new FileReader(fileName));
             Scanner scan = new Scanner(new FileReader(fileName));
@@ -80,7 +80,7 @@ public class Maze {
                 columns = find.length();
                 rows++;
             }
-             maze = new char[rows][columns];
+            maze = new char[rows][columns];
 
             System.out.println(rows + " " + columns);
             while (scanner.hasNextLine()) {
@@ -94,36 +94,66 @@ public class Maze {
                 System.out.println();
                 for (int k = 0; k < columns; k++) {
                     System.out.print(maze[j][k]);
-                } }
+                }
+            }
             return maze;
-            
+
         } catch (Exception e) {
             System.out.println(e);
             e.printStackTrace();
             return null;
         }
     }
-    public void print(){
+
+    public boolean checkPos() {//checks if the maze is possible
         for (int j = 0; j < rows; j++) {
-                System.out.println();
-                for (int k = 0; k < columns; k++) {
-                    System.out.print(maze[j][k]);
-                } }
-        
+            for (int k = 0; k < columns; k++) {
+                if (maze[j][k] == '+') {
+                    return true;
+                }
+            }
+        }
+        System.out.println("This maze is impossible");
+        return false;
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    public void print() {//gets rid of breadcrumbs and prints completed maze
+        maze[startR][startC] = 'S';
+        for (int l = 0; l < rows; l++) {
+
+            for (int m = 0; m < columns; m++) {
+                if (maze[l][m] == '.') {
+                    maze[l][m] = ' ';
+
+                }
+            }
+        }
+
+        for (int j = 0; j < rows; j++) {
+            System.out.println();
+            for (int k = 0; k < columns; k++) {
+                System.out.print(maze[j][k]);
+            }
+        }
+
+    }
+
+    
     public static void main(String[] args) {
         Maze maze = new Maze();
-        maze.ReadTextFile();
-       System.out.println( maze.SolveMaze(maze.FindStartR(), maze.FindStartC()));
-        maze.print();
-        // TODO code application logic here
+        maze.ReadTextFile("C:/Users/Nevin/Documents/testtextfile.txt");
+        maze.SolveMaze(maze.FindStartR(), maze.FindStartC());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        if (maze.checkPos()) {
+            maze.print();
+        }
+        
+
        
-        // System.out.println(board[0][1]);
     }
 
 }
+ 
 
